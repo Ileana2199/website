77b0b16847193261d1a41a61ab4cfb5b4db9e2ad
ileana2199 <ileanapintalucas@gmail.com>
2023-05-04 19:41:09 -0500

    Actualización del proyecto Sweet-alert y borrado lógico

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -78,13 +78,13 @@ var url=e.currentTarget.getAttribute('href')
 
 
 Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
+  title: '¿Estas seguro?',
+  text: "Este registro se eliminará definitivamente",
   icon: 'warning',
   showCancelButton: true,
   confirmButtonColor: '#3085d6',
   cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
+  confirmButtonText: '¡Si, eliminar!'
 }).then((result) => {
   if (result.isConfirmed) {
     window.location.href =url;
